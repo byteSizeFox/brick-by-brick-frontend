80597e4eb8cf25e0c99fdd57990b8ca17cd76441
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-11-07 14:25:19 -0800

    Added post protected show page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import AboutUs from './pages/AboutUs'   
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import PostEdit from './pages/PostEdit'
 import PostIndex from './pages/PostIndex'
 import PostNew from './pages/PostNew'
 import PostProtectedIndex from './pages/PostProtectedIndex'
+import PostProtectedShow from './pages/PostProtectedShow'
 import PostShow from './pages/PostShow'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -46,9 +48,11 @@ function App() {
             <Route path="/postindex" element={<PostIndex posts={posts} />} />
             <Route path="/postnew" element={<PostNew createPost={createPost} />} />
             <Route path="/myposts" element={<PostProtectedIndex currentUser={currentUser} posts={posts} />} />
+            <Route path="/mypostshow/:id" element={<PostProtectedShow currentUser={currentUser} posts={posts} />} />
             <Route path="/postshow/:id" element={<PostShow posts={posts} />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp signup={SignUp} />} />
+            <Route path="/aboutus" element={<AboutUs />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
